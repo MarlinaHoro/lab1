14cd0f6091024b67ac5730d60a3bcfed3db52e73
marlina <marlina1@gmail.com>
2020-10-30 12:52:11 +0700

    file biodata

---
+++ Biodata.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+nama = input("masukan nama: ")
+nama_panggilan = input ("masukan nama panggilan: ")
+npm = input ("masukan npm: ")
+alamat = input("masukan alamat: ")
+tpl = input("masukan tempat lahir: ") 
+tgl = input("masukan tanggal lahir: ")
+umr = input ("masukan umur: ")
+tlfn = input ("masukan nomor telefon: ")
+
+
+
+print("nama saya",nama, "nama panggilan",nama_panggilan,"npm saya adalah",npm,"alamat saya dikota",alamat,"tempat lahir saya dikota",tpl,"tanggal lahir saya",tgl,"umur saya",umr,"tahun" "nomor telefon saya",tlfn)
